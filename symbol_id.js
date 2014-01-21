9069a4cfdbbb5724f0a622c3d3d3c76762b9f190
Quinn Slack <qslack@qslack.com>
2014-01-21 10:19:55 -0800

    Rm log

--- symbol_id.js
+++ symbol_id.js
@@ -5,6 +5,5 @@ exports.parse = function(path) {
   var parts = path.split('.');
   var namespace = parts[0].slice(1);
   if (namespace == 'node') namespace = 'commonjs';
-  if (!parts[1]) console.error(parts);
   return {namespace: namespace, module: parts[1].replace(/`/g, '.'), path: parts.slice(2).join('.')};
 }
